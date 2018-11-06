6e824067817fe27d16e575a93bd9d3e05766b8e1
root <root@localhost.localdomain>
2018-11-06 14:39:50 +0100

    add 1a-add.py

---
+++ 1a-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python36
+
+
+nbr1 = int(input('premier nombre a ajouter : '))
+nbr2 = int(input('deuxieme nombre a ajouter : '))
+
+if type(nbr1) == int:
+  somme = nbr1 + nbr2
+  print(somme)
+
